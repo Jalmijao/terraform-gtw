e432f120751aaf161364995e2e68e914e6faaff0
james <jalmiriago@usp.br>
2025-07-05 17:25:09 -0300

    .

--- main.tf
+++ main.tf
@@ -17,7 +17,6 @@ resource "aws_api_gateway_rest_api" "my_api" {
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [aws_api_gateway_rest_api.my_api]
   rest_api_id = aws_api_gateway_rest_api.my_api.id
-  stage_name  = "prod"
 }
 
 resource "aws_api_gateway_stage" "prod_stage" {

06b162439fb59da3c6ede1700d436aadb3acc2e8
james <jalmiriago@usp.br>
2025-07-05 18:02:06 -0300

    .

--- main.tf
+++ main.tf
@@ -2,20 +2,21 @@ provider "aws" {
   region = "sa-east-1"
 }
 
-# Read your OpenAPI spec file
 data "local_file" "openapi_spec" {
   filename = "${path.module}/api-specification.yaml"
 }
 
 resource "aws_api_gateway_rest_api" "my_api" {
   name = "terraformado-api"
-
-  # Import the openapi spec body as a string
   body = data.local_file.openapi_spec.content
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.my_api.id
+
+  triggers = {
+    redeploy = sha1(data.local_file.openapi_spec.content)
+  }
 
   lifecycle {
     create_before_destroy = true
@@ -25,6 +26,7 @@ resource "aws_api_gateway_deployment" "deployment" {
     aws_api_gateway_rest_api.my_api
   ]
 }
+
 resource "aws_api_gateway_stage" "prod_stage" {
   stage_name    = "prod"
   rest_api_id   = aws_api_gateway_rest_api.my_api.id

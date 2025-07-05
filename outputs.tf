ecf15e255ff631c03e82d26ec603b8c7c6dec595
james <jalmiriago@usp.br>
2025-07-05 17:29:00 -0300

    .

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,3 @@ output "api_id" {
   description = "API Gateway ID"
   value       = aws_api_gateway_rest_api.my_api.id
 }
-
-output "stage_name" {
-  description = "Deployment stage name"
-  value       = aws_api_gateway_stage.dev_stage.name
-}
